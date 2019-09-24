c66d9e2f06004cca13308cc7d00eca06112ea8ce
Isaac Tay <IsaacTay@protonmail.com>
2019-09-24 04:04:15 +0000

    Fixed syntax of start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-pip install jupyterlab
-pip install tensorflow-gpu==2.0.0-beta1 -q
+pip3 install jupyterlab -q
+pip3 install tensorflow-gpu==2.0.0-beta1 -q
 
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -d warp-transducer ]; then
   git clone https://github.com/HawkAaron/warp-transducer
   export CUDA_HOME=/usr/local/cuda; cd warp-transducer; mkdir build; cd build; cmake -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ..; make
-  export CUDA_HOME=/usr/local/cuda; cd /content/warp-transducer/tensorflow_binding; python setup.py install
-  rm -r warp-transducer
+  export CUDA_HOME=/usr/local/cuda; cd ../tensorflow_binding; python3 setup.py install
+  cd ../..
 fi
 
 jupyter nbconvert --to python ODT.ipynb
-python3 ODT.ipynb
+python3 ODT.py
