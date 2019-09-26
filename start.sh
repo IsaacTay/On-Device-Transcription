81159719b52ca922aac1269d916a8bf2741eec39
Isaac Tay <IsaacTay@protonmail.com>
2019-09-26 10:14:05 +0800

    Improved start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 pip3 install jupyterlab -q
-pip3 install tensorflow-gpu==2.0.0-beta1 -q
+
+TF_INSTALL=${TF_INSTALL:-2}
+if [ ${TF_INSTALL} -eq 1 ]; then
+  pip3 install tensorflow==2.0.0-beta1 -q
+elif [ ${TF_INSTALL} -eq 2 ]; then
+  pip3 install tensorflow-gpu=2.0.0
+fi
 
 if [ ! -d warp-transducer ]; then
   git clone https://github.com/HawkAaron/warp-transducer
-  export CUDA_HOME=/usr/local/cuda; cd warp-transducer; mkdir build; cd build; cmake -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ..; make
-  export CUDA_HOME=/usr/local/cuda; cd ../tensorflow_binding; python3 setup.py install
+  export CUDA_HOME=/usr/local/cuda; cd warp-transducer; mkdir build; cd build; cmake -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME ..; make
+  cd ../tensorflow_binding; pip3 install -e .
   cd ../..
 fi
 
